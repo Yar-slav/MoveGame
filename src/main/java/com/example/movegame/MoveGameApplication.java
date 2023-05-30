79305c6383742eeefb63200a2dad639f83c4e261
yfedyna <fedynayaroslav@gmail.com>
2023-05-30 15:55:12 +0300

    init

---
+++ src/main/java/com/example/movegame/MoveGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.movegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoveGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/movegame/MoveGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
